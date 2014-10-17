aaa12ae3f3bdf77aec8b3cc001ecfd2d7a4b3a19
Andru Shurov <shurov9009@gmail.com>
2014-10-17 11:53:37 +0400

    added create_pass_of_syllables

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,12 +2,10 @@
 class  PasswordController < ApplicationController
 
     def index
-
     end
 
     def get_password
-        pass = Passgen.new.create_pass;
-        render json: { succces: true, password: pass  }
+        render json: { succces: true, password: Passgen.new.create_pass_of_syllables }
     end
 
 end
